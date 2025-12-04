c75e0db9f490641782440bb1294499b2a555cf37
JoaoP3dr0o <joao.contato.97@hotmail.com>
2025-12-04 00:47:59 -0300

    <feat>[auth]:configurando tymon

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,23 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 
 // Route::get('/users', [UserController::class, 'index']);
 // Route::post('/users', [UserController::class, 'store']);
 Route::apiResource('/users', UserController::class);
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me']);
+
+});
